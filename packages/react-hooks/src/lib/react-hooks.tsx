c580d093300237ca26ead3fc98f6948947c0f45e
David Edwards <davidedwards.tech16@gmail.com>
2022-10-30 13:18:44 -0400

    repo: added react-hooks library

---
+++ packages/react-hooks/src/lib/react-hooks.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ReactHooksProps {}
+
+export function ReactHooks(props: ReactHooksProps) {
+  return (
+    <div>
+      <h1>Welcome to ReactHooks!</h1>
+    </div>
+  );
+}
+
+export default ReactHooks;
